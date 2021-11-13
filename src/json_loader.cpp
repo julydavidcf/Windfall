991eebbcb24a38702109c78b13275074bdafffee
Burcu Gorgulicten <burcug@students.cs.ubc.ca>
2021-11-12 23:59:51 -0800

    Iterate through the directories, find src, get the json file

--- src/json_loader.cpp
+++ src/json_loader.cpp
@@ -25,37 +25,53 @@ JSONLoader::JSONLoader() {
 }
 
 
-string get_path(string file_name){
+std::ifstream get_file(string file_name){
     // https://www.tutorialspoint.com/find-out-the-current-working-directory-in-c-cplusplus
     char buff[FILENAME_MAX]; //create string buffer to hold path
     GetCurrentDir( buff, FILENAME_MAX );
-    // https://java2blog.com/check-if-string-contains-substring-cpp/
-    char* pos = buff;
-    int index = 0;
-    while((pos[0]!='0')&&(pos[1]!='8')){
-        pos++;
-        index++;
+
+    // Convert the char array to a string
+    string curr_path;
+    for(int i = 0; i<FILENAME_MAX; i++){
+        if(buff[i] == '\0'){
+            break;
+        }
+        curr_path = curr_path + buff[i];
     }
-    pos = pos + 2;
-    index = index + 2;
-    string src =  "/src/";
-    string add_string = src.append(file_name);
-    string new_path = "";
-    for(int i = 0; i<index; i++){
-        new_path = new_path+buff[i];
+
+    // Get the full path to the json file
+    string iterate_path = curr_path;
+    while(true){
+        string try_file = iterate_path;
+        #ifdef _WIN64
+            try_file.append("\\..\\src\\").append(file_name);
+        #elif _WIN32
+            try_file.append("\\..\\src\\").append(file_name);
+        #else
+            try_file.append("/../src/").append(file_name);
+        #endif
+        std::ifstream jsonFile(try_file);
+        if (jsonFile) {
+            return jsonFile;
+        }
+        #ifdef _WIN64
+            iterate_path = iterate_path.append("\\..").append(file_name);
+        #elif _WIN32
+            iterate_path = iterate_path.append("\\..").append(file_name);
+        #else
+            iterate_path = iterate_path.append("/..").append(file_name);
+        #endif
     }
-    new_path = new_path.append(add_string);
-    return new_path;
 }
 
 void JSONLoader::get_level(){
-    string path = get_path("small_example.json");
-    std::ifstream jsonFile(path);
-    cout << "Current working directory: " << path << endl;
+    std::ifstream jsonFile = get_file("small_example.json");
     if (jsonFile.is_open()){
-        //std::cout << jsonFile.rdbuf();
+        // std::cout << "PRINTING"<< endl;
+        // std::cout << jsonFile.rdbuf();
 	}
 	else {printf("not open?\n");}
+    get_file("small_example.json");
     json j = json::parse(jsonFile);
     std::cout << j << std::endl;
 }
