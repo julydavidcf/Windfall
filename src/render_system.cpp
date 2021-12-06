2d4ab12fcdcc06c193c4905868c61a541b274af1
Burcu Gorgulicten <burcug@students.cs.ubc.ca>
2021-12-05 21:32:09 -0800

    Small fix in render

--- src/render_system.cpp
+++ src/render_system.cpp
@@ -1048,7 +1048,7 @@ void RenderSystem::draw(float elapsed_ms)
 
 	drawToScreen();
 
-	if (isFreeRoam == 2) {
+	if (isFreeRoam && (freeRoamLevel == 2)) {
 		for (int i = 0; i < registry.motions.components.size(); i++) {
 			Entity e = registry.motions.entities[i];
 			if (registry.light.has(e)) {
