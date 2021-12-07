4bd2dce8e46326ea5188e75e4799bd8f5fadbc1f
julydavidcf <50227264+julydavidcf@users.noreply.github.com>
2021-12-06 17:03:03 -0800

    small change, arrow bug fix

--- src/world_system.cpp
+++ src/world_system.cpp
@@ -2134,6 +2134,7 @@ void WorldSystem::handle_collisions()
 				if (registry.bird.has(entity_other))
 				{
 					registry.remove_all_components_of(entity_other);
+					registry.remove_all_components_of(entity);
 					Mix_PlayChannel(-1, registry.crow_sound, 0);
 				}
 
